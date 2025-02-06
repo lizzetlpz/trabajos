f4bbd910539e24f35761fc6795cda96a682830a0
lizzetlpz <lizzlpzza@gmail.com>
2025-02-06 14:42:10 -0700

    Create Introduccion.ps1

---
+++ Introduccion.ps1
@@ -0,0 +1,22 @@
+$Variable1="Hola"
+$Variable2="Que tal?"
+$variable3=100
+${VAR iable4}=200
+New-Variable -Name variable5 -Value 300
+$variable5
+$Variable1
+$Variable2
+$variable3
+${VAR iable4}
+$Variable1 + $Variable2
+$variable3+${VAR iable4}
+$variable3-${var iable4}
+$Variable1+$Variable2
+$$
+$variable1 + $variable2
+$^
+$Variable1 + $Variable2
+$?
+$Error
+Get-Help about_automatic_variables
+get-help about_preference_variables
